e408f04a1db88ced43d476eed240ca59b9f17a2b
kiss8981 <kim@dohyun.kr>
2023-12-11 05:42:39 +0000

    feat: blind article

---
+++ src/app/(afterLoginNoNavigation)/board/[boardId]/[articleId]/report/[reportId]/page.tsx
@@ -0,0 +1,46 @@
+import HeaderContainer from "@/app/_component/HeaderContainer";
+import { authOptions } from "@/app/auth";
+import fetcher from "@/lib/fetch";
+import { Session } from "next-auth";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { Report } from "schoolmate-types";
+import ReportComponent from "./_component/ReportComponent";
+
+interface Props {
+  params: {
+    articleId: string;
+    boardId: string;
+    reportId: string;
+  };
+}
+
+const getReportResult = async (reportId: string, auth: Session) => {
+  const article = await fetcher(`/report/${reportId}`, {
+    headers: {
+      Authorization: `Bearer ${auth.user.token.accessToken}`,
+    },
+  });
+  if (article.data.status === 401) return redirect("/intro");
+  if (article.data.status === 404) return null;
+
+  return article.data.data as Report;
+};
+
+const ReportResultPage = async ({ params }: Props) => {
+  const auth = await getServerSession(authOptions);
+
+  if (!auth || !auth.user.registered) return redirect("/intro");
+  if (!auth.user.user.userSchool) return redirect("/verify");
+  const report = await getReportResult(params.reportId, auth);
+  if (!report) return <></>;
+  return (
+    <>
+      <HeaderContainer title="추가조치">
+        <ReportComponent report={report} auth={auth} />
+      </HeaderContainer>
+    </>
+  );
+};
+
+export default ReportResultPage;
