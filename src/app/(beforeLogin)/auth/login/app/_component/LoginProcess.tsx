dcb11dc218f39026ac7301f754aa2ced53940d5b
kiss8981 <kim@dohyun.kr>
2024-02-08 20:21:10 +0000

    fix(auth): update session user

--- src/app/(beforeLogin)/auth/login/app/_component/LoginProcess.tsx
+++ src/app/(beforeLogin)/auth/login/app/_component/LoginProcess.tsx
@@ -7,7 +7,7 @@ import { sendWebviewEvent, toast } from "@/lib/webviewHandler";
 
 import { useRouter } from "next/navigation";
 import React, { useEffect, useLayoutEffect } from "react";
-import { getSession, signIn, useSession } from "next-auth/react";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
 import { authOptions } from "@/app/auth";
 
 const LoginProcess: React.FC<{
@@ -15,41 +15,45 @@ const LoginProcess: React.FC<{
 }> = ({ token }) => {
   const router = useRouter();
   useEffect(() => {
-    signIn("credentials", {
-      redirect: false,
-      code: token,
-      provider: "app",
-    }).then(async res => {
-      if (!res?.ok) {
-        toast("error", "로그인 세션이 만료되었습니다.");
-        router.replace("/auth/login");
-      } else {
-        const session = await getSession();
-        if (!session?.user) {
-          toast("error", "로그인에 실패하였습니다.");
-          router.replace("/auth/login");
-          return;
-        }
+    (async () => {
+      await signOut({ redirect: false }).finally(() => {
+        signIn("credentials", {
+          redirect: false,
+          code: token,
+          provider: "app",
+        }).then(async res => {
+          if (!res?.ok) {
+            toast("error", "로그인 세션이 만료되었습니다.");
+            router.replace("/auth/login");
+          } else {
+            const session = await getSession();
+            if (!session?.user) {
+              toast("error", "로그인에 실패하였습니다.");
+              router.replace("/auth/login");
+              return;
+            }
 
-        setCookie("Authorization", session?.user.token.accessToken, {
-          path: "/",
-          maxAge: 60 * 60 * 24 * 7,
-          domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+            setCookie("Authorization", session?.user.token.accessToken, {
+              path: "/",
+              maxAge: 60 * 60 * 24 * 7,
+              domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+            });
+
+            if (window.ReactNativeWebView) {
+              sendWebviewEvent("LOGIN_EVENT", {
+                type: "callback",
+                loginType: "phone",
+                token: {
+                  accessToken: session?.user.token.accessToken,
+                  refreshToken: session?.user.token.refreshToken,
+                },
+              });
+            }
+            router.replace("/main");
+          }
         });
-
-        if (window.ReactNativeWebView) {
-          sendWebviewEvent("LOGIN_EVENT", {
-            type: "callback",
-            loginType: "phone",
-            token: {
-              accessToken: session?.user.token.accessToken,
-              refreshToken: session?.user.token.refreshToken,
-            },
-          });
-        }
-        router.replace("/main");
-      }
-    });
+      });
+    })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
